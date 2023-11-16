df221dcc61f65dc5d7b10e2a74ae25a1466724c6
Aaron A. Brown <kilonow1275@hotmail.com>
2023-11-15 19:53:53 -0700

    fixed css for chatbot modal sizing and positioning  issues along with child elements

--- server/client/src/components/Chatbot.js
+++ server/client/src/components/Chatbot.js
@@ -1,15 +1,67 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef, useCallback } from "react";
 import { connect } from "react-redux";
 import { sendMessage, addUserMessage } from "../actions"; // Correct this path
 
 function Chatbot({ messages, sendMessage, addUserMessage, username }) {
   const [showChat, setShowChat] = useState(false);
   const messagesEndRef = useRef(null); // Ref for the messages container
+  const [isDragging, setIsDragging] = useState(false);
+  const chatbotRef = useRef(null);
+  const [position, setPosition] = useState({ x: 0, y: 0 }); 
+  const [dragOffset, setDragOffset] = useState({ x: 0, y: 0 });
+  
  
+  useEffect(() => {
+    // Set the initial position after the component has mounted
+    
+    setPosition({ x: window.innerWidth - 350, y: window.innerHeight - 500 });
+  }, []);
+  
+ 
+  const handleMouseDown = (e) => {
+    setIsDragging(true);
+    // Calculate the offset from the mouse position to the top-left corner of the chatbot
+    // const rect = chatbotRef.current.getBoundingClientRect();
+    setDragOffset({
+       x: e.clientX - position.x,
+      y: e.clientY - position.y,
+    });
+    // chatbotRef.current.style.position = 'fixed';
+  };
+
+
+  const handleMouseMove = useCallback((e) => {
+    if (isDragging) {
+      // Calculate new position
+    let newX = e.clientX - dragOffset.x;
+    let newY = e.clientY - dragOffset.y;
+
+    // Get viewport dimensions
+    const screenWidth = window.innerWidth;
+    const screenHeight = window.innerHeight;
+
+    // Get chatbot dimensions
+    const chatbotWidth = chatbotRef.current.offsetWidth;
+    const chatbotHeight = chatbotRef.current.offsetHeight;
+
+    // Ensure the chatbot stays within the bounds of the viewport
+    newX = Math.max(0, Math.min(newX, screenWidth - chatbotWidth));
+    newY = Math.max(0, Math.min(newY, screenHeight - chatbotHeight));
+
+    setPosition({ x: newX, y: newY });
+    }
+  }, [isDragging, dragOffset.x, dragOffset.y]);
+
+
+   const handleMouseUp = () => {
+    setIsDragging(false);
+  };
 
   const scrollToBottom = () => {
     messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
   };
+
+  
 
   useEffect(() => {
     scrollToBottom();  // Scroll to bottom whenever messages update
@@ -21,12 +73,26 @@ function Chatbot({ messages, sendMessage, addUserMessage, username }) {
     sendMessage(message);
   };
 
-  
+   useEffect(() => {
+    window.addEventListener("mousemove", handleMouseMove);
+    window.addEventListener("mouseup", handleMouseUp);
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+      window.removeEventListener("mouseup", handleMouseUp);
+    };
+  }, [isDragging, handleMouseMove]);
 
-  
+ 
+
+    
 
   return (
-    <div className="chatbot">
+    <div className="chatbot"
+    ref={chatbotRef}
+    onMouseDown={handleMouseDown}
+    style={{left: `${position.x}px`, top: `${position.y}px`, 
+           position: 'fixed',
+           cursor: isDragging ? 'grabbing' : 'grab'}}>
       {!showChat
         ? <button className="chatbot-trigger" onClick={() => setShowChat(true)}>
             Open Chatbot
@@ -38,7 +104,7 @@ function Chatbot({ messages, sendMessage, addUserMessage, username }) {
             >
               Close (X)
             </button>
-          <div className="resize-grip"></div>
+          <div className="resize-grip" ></div>
             
             <div className="messages">
               {messages.map((msg, index) =>
@@ -64,8 +130,8 @@ function Chatbot({ messages, sendMessage, addUserMessage, username }) {
                 </div>
               )}
             </div>
-            
-            <input
+            <div className="input-container">
+              <input
               type="text"
               onKeyDown={e => {
                 if (e.key === "Enter") {
@@ -74,9 +140,10 @@ function Chatbot({ messages, sendMessage, addUserMessage, username }) {
                 }
               }}
               placeholder="Type your message..."
-            />
-            <span className="powered-by">Powered by ChatGPT</span>
-          </div>}
+            />            
+            </div>
+            <span className="powered-by">Powered by ChatGPT</span>   
+          </div>}          
     </div>
   );
 }

d9ce24df4cacef0095bb96fd33b4b831de273338
Aaron A. Brown <kilonow1275@hotmail.com>
2024-08-21 20:02:06 -0600

    added vercel.json adn arrange folder routes under api folder to conform to vercel build rules

--- server/client/src/reducers/authReducer.js
+++ server/client/src/reducers/authReducer.js
@@ -1,7 +1,12 @@
 // Authreducer: decides whether a user is logged in or not
 // allows us to access : this.props.auth - user model instance and its properties ie. credits
 import { FETCH_USER } from "../actions/types";
-export default function(state = null, action) {
+
+const initialState = {
+  user: null,
+  projectsTracked: [] // Initialize projectsTracked as an empty array
+};
+export default function(state = initialState, action) {
   switch (action.type) {
     case FETCH_USER:
       return action.payload || false; // if payload is empty string, return false. If payload is not empty string, return payload.
